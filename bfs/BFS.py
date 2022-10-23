81be0ba17b35cafe1e65c720c36874b5f3ff8033
Danial <Danialsb778@gmail.com>
2022-10-23 22:16:56 +0330

    Initial commit

---
+++ bfs/BFS.py
@@ -0,0 +1,54 @@
+from pickle import NONE
+from queue import Queue
+
+adjacency_list = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+class Graph:
+
+    def __init__(self):
+        self.visited = {}
+        self.level = {}
+        self.parent = {}
+        self.queue = Queue()
+        self.bfs_traversal_output = []
+
+
+    def bfs(self):
+
+        for node in adjacency_list.keys():
+            self.visited[node] = False
+            self.level[node] = -1
+            self.parent[node] = NONE
+        
+        root = '5'
+        self.visited[root] = True
+        self.level[root] = 0
+        self.queue.put(root)
+
+        while not self.queue.empty():
+            u = self.queue.get()
+            self.bfs_traversal_output.append(u)
+
+            for v in adjacency_list[u]:
+                if not self.visited[v]:
+                    self.visited[v] = True
+                    self.parent[v] = u
+                    self.level[v] = self.level[u] + 1
+                    self.queue.put(v)
+
+        return self.bfs_traversal_output
+
+
+
+
+if __name__ == "__main__":
+    g = Graph()
+    print(g.bfs())
+    print(g.level)
